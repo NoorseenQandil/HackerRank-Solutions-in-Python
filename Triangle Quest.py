dfa0e8321eaa9b37a31b35baa22c6364ac805424
Nourseen Qandil <70522199+NoorseenQandil@users.noreply.github.com>
2022-10-02 17:23:17 +0200

    Update Triangle Quest.py

--- Triangle Quest.py
+++ Triangle Quest.py
@@ -1,3 +1,3 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-for i in range(1,int(input())): 
+for i in range(1,int(input())):  #More than 2 lines will result in 0 score.Do not leave a blank line also.
     print(((10**i) // 9) * i)

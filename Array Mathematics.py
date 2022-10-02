2bcaac4019bc5ee40bfefd5a8221c51e28d0134a
Nourseen Qandil <70522199+NoorseenQandil@users.noreply.github.com>
2022-10-02 15:54:29 +0200

    Create Array Mathematics.py

---
+++ Array Mathematics.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import numpy as np
+
+m, n = map(int, input().split())
+a = np.array([list(map(int, input().split())) for _ in range(m)], np.int64)
+b = np.array([list(map(int, input().split())) for _ in range(m)], np.int64)
+print(f"{a+b}\n{a-b}\n{a*b}\n{a//b}\n{a%b}\n{a**b}")
